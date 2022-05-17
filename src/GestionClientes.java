ea5b5711f6a6259a0c90f321c82f00a967187073
AntGiRe <101841460+AntGiRe@users.noreply.github.com>
2022-05-17 21:00:14 +0200

    Programa acepta cualquier BBDD

--- src/GestionClientes.java
+++ src/GestionClientes.java
@@ -1,40 +1,68 @@
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import base_datos.DBManager;
 
 /**
  *
- * @author lionel
+ * @author Antonio J. Gil
+ * created on 10/05
+ * @version 3.0
  */
 public class GestionClientes {
 
+	/**
+	 * Main
+	 * @param args Argumentos
+	 */
     public static void main(String[] args) {
-
-        DBManager.loadDriver();
-        DBManager.connect();
-
-        boolean salir = false;
+    	boolean baseCorrecta = false;
+    	
+    	do {
+    		pideBase();
+    		
+    		try {
+    			baseCorrecta = DBManager.connect();
+    		} catch (SQLException e) {
+    			System.out.println(" [" + e + "] - No se pudo conectar con la BBDD, intentelo de nuevo.\n");
+    		}
+    		
+    	}while(!baseCorrecta);
+    	
+    	System.out.println("OK!");	
+    	
+    	eligeTabla();
+    		
+    	boolean salir = false;
         do {
-            salir = menuPrincipal();
+        	salir = menuPrincipal();
         } while (!salir);
-
-        DBManager.close();
+            
+        DBManager.close();	
 
     }
 
+    /**
+     * Imprime menú y solicita elegir opción
+     * @return devuelve false cuando el usuario sale del menú principal
+     */
     public static boolean menuPrincipal() {
         System.out.println("");
         System.out.println("MENU PRINCIPAL");
-        System.out.println("1. Listar clientes");
-        System.out.println("2. Nuevo cliente");
-        System.out.println("3. Modificar cliente");
-        System.out.println("4. Eliminar cliente");
-        System.out.println("5. Salir");
+        System.out.println("0. Salir");	//DONE
+        System.out.println("1. Listar filas");	//DONE
+        System.out.println("2. Nueva fila");	//DONE
+        System.out.println("3. Modificar fila"); //DONE
+        System.out.println("4. Eliminar fila");	//DONE
+        System.out.println("5. Imprimir tabla en un fichero");	//DONE
+        System.out.println("6. Cambiar la BBDD");	//DONE
         
-        Scanner in = new Scanner(System.in);
-            
         int opcion = pideInt("Elige una opción: ");
         
         switch (opcion) {
+        	case 0:
+        		return true;
             case 1:
                 opcionMostrarClientes();
                 return false;
@@ -48,7 +76,18 @@ public class GestionClientes {
                 opcionEliminarCliente();
                 return false;
             case 5:
-                return true;
+            	opcionImprimirFichero();
+            	return false;
+            case 6:
+            	pideBase();
+            	eligeTabla();
+            	try {
+            		DBManager.connect();
+            		System.out.println("OK!");
+            	} catch (SQLException e) {
+            		System.out.println("No se pudo conectar a la BBDD");
+            	}
+            	return false;
             default:
                 System.out.println("Opción elegida incorrecta");
                 return false;
@@ -56,27 +95,37 @@ public class GestionClientes {
         
     }
     
+    /**
+     * Solicita un entero
+     * @param mensaje Cadena de texto que pide un valor determinado al usuario
+     * @return devuelve un entero
+     */
     public static int pideInt(String mensaje){
-        
+    	Scanner in = new Scanner(System.in);
+    	
         while(true) {
             try {
                 System.out.print(mensaje);
-                Scanner in = new Scanner(System.in);
                 int valor = in.nextInt();
-                //in.nextLine();
                 return valor;
             } catch (Exception e) {
+            	in.nextLine();
                 System.out.println("No has introducido un número entero. Vuelve a intentarlo.");
             }
         }
+        
     }
     
+    /**
+     * Solicita una cadena de texto
+     * @param mensaje Cadena de texto que pide un valor determinado al usuario
+     * @return devuelve una cadena de texto
+     */
     public static String pideLinea(String mensaje){
-        
+    	Scanner in = new Scanner(System.in);
         while(true) {
             try {
                 System.out.print(mensaje);
-                Scanner in = new Scanner(System.in);
                 String linea = in.nextLine();
                 return linea;
             } catch (Exception e) {
@@ -84,32 +133,69 @@ public class GestionClientes {
             }
         }
     }
+    
+    /**
+     * Solicita nueva BBDD, tabla y campos a las que se acceden
+     */
+    public static void pideBase(){
+    	
+    	String base = pideLinea("Introduce el nombre de la base de datos a la que acceder: ");
+    	
+    	DBManager.cambioBaseNueva(base);
+    	
+    }
+    
+    /**
+     * Permite elegir una tabla de la BD
+     */
+    public static void eligeTabla(){
+    	
+    	System.out.println(" ** TABLAS **");
+    	DBManager.listarTablas();
+    	DBManager.setTabla(pideLinea("Elige una tabla: "));
+    	
+    }
+    
+    
 
+    /**
+     * Imprime listado de clientes
+     */
     public static void opcionMostrarClientes() {
-        System.out.println("Listado de Clientes:");
+        System.out.println("Listado de tabla:");
         DBManager.printTablaClientes();
     }
-
+    
+    /**
+     * Introducir nuevo cliente a la BBDD
+     */
     public static void opcionNuevoCliente() {
-        Scanner in = new Scanner(System.in);
+    	ArrayList<String> lista = new ArrayList<String>();
+    	
+    	for(int i = 1; i <= DBManager.numeroColumnas(); i++) {
+    		if(DBManager.nombreColumna(i).equals(DBManager.getPK())) {
+    			lista.add(null);
+    		} else {
+    			lista.add(pideLinea("Introduce el " + DBManager.nombreColumna(i) + ": "));
+    		}
+    		
+    	}
 
-        System.out.println("Introduce los datos del nuevo cliente:");
-        String nombre = pideLinea("Nombre: ");
-        String direccion = pideLinea("Dirección: ");
-
-        boolean res = DBManager.insertCliente(nombre, direccion);
+        boolean res = DBManager.insertCliente(lista);
 
         if (res) {
-            System.out.println("Cliente registrado correctamente");
+            System.out.println("Fila registrada correctamente");
         } else {
             System.out.println("Error :(");
         }
     }
 
+    /**
+     * Modifica los datos de un cliente de una BBDD
+     */
     public static void opcionModificarCliente() {
-        Scanner in = new Scanner(System.in);
 
-        int id = pideInt("Indica el id del cliente a modificar: ");
+        int id = pideInt("Indica el id de la fila a modificar: ");
 
         // Comprobamos si existe el cliente
         if (!DBManager.existsCliente(id)) {
@@ -121,27 +207,28 @@ public class GestionClientes {
         DBManager.printCliente(id);
 
         // Solicitamos los nuevos datos
-        String nombre = pideLinea("Nuevo nombre: ");
-        String direccion = pideLinea("Nueva dirección: ");
+        String aModificar = pideLinea("Que campo quieres modificar: ");
+        String nuevo = pideLinea("Nueva entrada de " + aModificar + ": ");
 
-        // Registramos los cambios
-        boolean res = DBManager.updateCliente(id, nombre, direccion);
+        boolean res = DBManager.updateCliente(id,aModificar, nuevo);
 
         if (res) {
-            System.out.println("Cliente modificado correctamente");
+            System.out.println("Fila modificada correctamente");
         } else {
             System.out.println("Error :(");
         }
     }
 
+    /**
+     * Eliminar un cliente de la BBDD
+     */
     public static void opcionEliminarCliente() {
-        Scanner in = new Scanner(System.in);
 
-        int id = pideInt("Indica el id del cliente a eliminar: ");
+        int id = pideInt("Indica el id de la fila a eliminar: ");
 
         // Comprobamos si existe el cliente
         if (!DBManager.existsCliente(id)) {
-            System.out.println("El cliente " + id + " no existe.");
+            System.out.println("La fila " + id + " no existe.");
             return;
         }
 
@@ -149,9 +236,28 @@ public class GestionClientes {
         boolean res = DBManager.deleteCliente(id);
 
         if (res) {
-            System.out.println("Cliente eliminado correctamente");
+            System.out.println("Fila eliminada correctamente");
         } else {
             System.out.println("Error :(");
         }
     }
+    
+    /**
+     * Solicita ruta donde generar fichero con clientes
+     */
+    public static void opcionImprimirFichero() {
+
+        String ruta = pideLinea("Indica la ruta o nombre (si quiere que se genere en la carpeta de proyecto) del fichero: ");
+
+        // Comprobamos si existe el cliente
+        if (DBManager.printClientesFichero(ruta)) {
+            System.out.println("El fichero se ha generado correctamente");
+            return;
+        } else {
+        	System.out.println("El fichero no se ha podido generar, revise lo introducido.");
+        	return;
+        }
+    }
+    
+    
 }
---
+++ src/base_datos/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * base_datos es un paquete que trabaja al mas bajo nivel con la Base de datos.
+ *
+ * @author Antonio J. Gil
+ * @version 3.0
+ */
+
+package base_datos;
